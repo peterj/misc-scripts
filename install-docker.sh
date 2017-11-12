50075fe24f9c4a5a13e9e80937beff31c113f456
peterj <>
2017-11-11 20:03:46 -0800

    Docker on Ubuntu, Traefik

---
+++ install-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Installs Docker and docker-compose on Ubuntu 16.04
+
+# Add the GPG key for the official Docker repository to the system
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the Docker repository to APT sources
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# Download docker-compose
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
---
+++ setup-traefic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker network create proxy
+touch acme.json
+chmod 600 acme.json
